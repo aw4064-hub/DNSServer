aeeb47e7598180631d8dc5b244773163aee70912
Alvin <aw4064@nyu.edu>
2025-10-25 23:22:36 -0400

    commit 4

--- DNSServer.py
+++ DNSServer.py
@@ -43,7 +43,7 @@ def decrypt_with_aes(encrypted_data, password, salt):
     decrypted_data = f.decrypt(encrypted_data) #call the Fernet decrypt method
     return decrypted_data.decode('utf-8')
 
-salt = bytes('Tandon', 'utf-8') # Remember it should be a byte-object
+salt = 'Tandon'.encode('utf-8') # Remember it should be a byte-object
 password = 'aw4064@nyu.edu'
 input_string = 'AlwaysWatching'
 
@@ -82,7 +82,7 @@ dns_records = {
 'yahoo.com': { dns.rdatatype.A:  '192.168.1.105'},
 'nyu.edu' : {
     dns.rdatatype.A: '192.168.1.106',
-    dns.rdatatype.TXT: ('a string cast version of your encrypted secret data from step 3',),
+    dns.rdatatype.TXT: str(encrypted_value),
     dns.rdatatype.MX: [(10, 'mxa-00256a01.gslb.pphosted.com.')],
     dns.rdatatype.AAAA: '2001:0db8:85a3:0000:0000:8a2e:0373:7312',
     dns.rdatatype.NS: 'ns1.nyu.edu.'
@@ -164,5 +164,5 @@ def run_dns_server_user():
 
 if __name__ == '__main__':
     run_dns_server_user()
-    print("Encrypted Value:", encrypted_value)
-    print("Decrypted Value:", decrypted_value)
+    # print("Encrypted Value:", encrypted_value)
+    # print("Decrypted Value:", decrypted_value)
